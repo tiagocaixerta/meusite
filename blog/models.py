1f23786e7568b2e2961bc4c3f851960d5e6a70c2
Tiago <caixerta63@gmail.com>
2025-05-28 09:49:07 -0300

    Adiciona suporte ao campo status no modelo Post e configura o admin

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,26 @@
 from django.db import models
+from django.utils.text import slugify
 
 class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Rascunho'),
+        ('published', 'Publicado'),
+    )
+
     titulo = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, blank=True)
     conteudo = models.TextField()
     criado_em = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default='draft'
+    )
 
     def __str__(self):
         return self.titulo
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.titulo)
+        super().save(*args, **kwargs)
