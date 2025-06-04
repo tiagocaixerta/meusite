310ffe53d76bfb66b0d8b6ab52b0fc13b9845a38
Tiago Caixerta <caixerta63@gmail.com>
2025-06-04 10:33:07 -0300

    Commit inicial com setup do projeto async_views

---
+++ asyncviews/views.py
@@ -0,0 +1,17 @@
+import asyncio
+import httpx
+from django.http import HttpResponse
+
+async def http_call_async():
+    for num in range(1, 6):
+        await asyncio.sleep(1)
+        print(num)
+
+    async with httpx.AsyncClient() as client:
+        r = await client.get("https://httpbin.org")
+        print(r)
+
+async def async_view(request):
+   
+    asyncio.create_task(http_call_async())
+    return HttpResponse("Non-blocking HTTP request!")
