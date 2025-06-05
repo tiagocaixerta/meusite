0f846626d3f07d339d86b3c71351c99acef7f8c2
TIAGO BARBOSA CAIXERTA <67751563+tiagocaixerta@users.noreply.github.com>
2025-06-05 08:16:18 -0300

    Update factories.py

--- blog/factories.py
+++ blog/factories.py
@@ -29,7 +29,7 @@ class PostFactory(factory.django.DjangoModelFactory):
     title = factory.LazyAttribute(lambda x: faker.sentence())
     created_on = factory.LazyAttribute(lambda x: now())
     author= factory.SubFactory(UserFactory)
-    status = 0
+    status = 'published'
 
     class Meta:
         model =  Post
