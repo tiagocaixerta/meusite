b4303a715fea1811805adde8e32cb40c6038afdd
TIAGO BARBOSA CAIXERTA <67751563+tiagocaixerta@users.noreply.github.com>
2025-06-06 08:07:44 -0300

    Update test_post.py

--- tests/models/test_post.py
+++ tests/models/test_post.py
@@ -1,11 +1,10 @@
 import pytest
-
 from blog.factories import PostFactory
 
 @pytest.fixture
 def post_published():
-    return PostFactory(title='pytest with factory')
+    return PostFactory(titulo='pytest with factory')
 
 @pytest.mark.django_db
 def test_create_published_post(post_published):
-    assert post_published.title == 'pytest with factory'
+    assert post_published.titulo == 'pytest with factory'
