d87c5cf97a961e98b4681e31be3db3ab4d722c01
TIAGO BARBOSA CAIXERTA <67751563+tiagocaixerta@users.noreply.github.com>
2025-06-06 08:01:04 -0300

    Update factories.py

--- blog/factories.py
+++ blog/factories.py
@@ -1,35 +1,15 @@
 import factory
 from faker import Factory as FakerFactory
 
-from django.contrib.auth.models import User
 from django.utils.timezone import now
-
 from blog.models import Post
 
 faker = FakerFactory.create()
 
-class UserFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = User
-
-    email = factory.Faker("safe_email")
-    username= factory.LazyAttribute(lambda x: faker.name())
-
-    @classmethod
-    def _prepare(cls, create, **kwargs):
-        password = kwargs.pop('password', None)   
-        user = super(UserFactory, cls)._prepare(create, **kwargs) 
-        if password:
-            user.set_password(password)
-            if create:
-                user.save()
-        return user
-
 class PostFactory(factory.django.DjangoModelFactory):
-    title = factory.LazyAttribute(lambda x: faker.sentence())
-    created_on = factory.LazyAttribute(lambda x: now())
-    author= factory.SubFactory(UserFactory)
-    status = 'published'
+    titulo = factory.LazyAttribute(lambda x: faker.sentence())
+    conteudo = factory.Faker("text")
+    criado_em = factory.LazyFunction(now)  # pode ser omitido, já que o modelo tem auto_now_add
 
     class Meta:
-        model =  Post
+        model = Post
