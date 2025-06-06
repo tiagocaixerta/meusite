0c32258be667d709e50b334543b1135d158cf2ad
TIAGO BARBOSA CAIXERTA <67751563+tiagocaixerta@users.noreply.github.com>
2025-06-06 08:02:06 -0300

    Update factories.py

--- blog/factories.py
+++ blog/factories.py
@@ -1,15 +1,14 @@
 import factory
 from faker import Factory as FakerFactory
-
 from django.utils.timezone import now
 from blog.models import Post
 
 faker = FakerFactory.create()
 
 class PostFactory(factory.django.DjangoModelFactory):
-    titulo = factory.LazyAttribute(lambda x: faker.sentence())
-    conteudo = factory.Faker("text")
-    criado_em = factory.LazyFunction(now)  # pode ser omitido, já que o modelo tem auto_now_add
+    titulo = factory.LazyAttribute(lambda x: faker.sentence(nb_words=6))
+    conteudo = factory.LazyAttribute(lambda x: faker.paragraph(nb_sentences=3))
+    criado_em = factory.LazyFunction(now)  
 
     class Meta:
         model = Post
